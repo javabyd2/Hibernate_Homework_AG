2e5e2b5c66077b0c4664f0e0e32438e76849e29a
Adrian Gielzak <adrian.gielzak@outlook.com>
2018-03-19 09:49:29 +0100

    create three tables

--- src/main/java/eu/gielzak/hibernate/Main.java
+++ src/main/java/eu/gielzak/hibernate/Main.java
@@ -1,9 +1,29 @@
 package eu.gielzak.hibernate;
 
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class Main {
 
-    public static void main(String[] args) {
+    private static final SessionFactory sessionFactory;
 
+    static {
+        try {
+            Configuration configuration = new Configuration();
+            configuration.configure();
+            sessionFactory = configuration.buildSessionFactory();
+        } catch (Throwable e) {
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+
+    public static Session getSession() throws HibernateException {
+        return sessionFactory.openSession();
+    }
+
+    public static void main(String[] args) {
 
 
     }
--- src/main/java/eu/gielzak/hibernate/entity/StudentsEntity.java
+++ src/main/java/eu/gielzak/hibernate/entity/StudentsEntity.java
@@ -1,4 +1,96 @@
 package eu.gielzak.hibernate.entity;
 
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Table(name = "students")
 public class StudentsEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true)
+    private int id;
+
+    @Column(nullable = false, length = 25)
+    private String first_name;
+
+    @Column(nullable = false, length = 55)
+    private String last_name;
+
+    private Date birthday;
+
+    @Column(length = 55)
+    private String address_street;
+
+    @Column(length = 55)
+    private String address_city;
+
+    private String pesel;
+
+    private String phone_number;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getAddress_street() {
+        return address_street;
+    }
+
+    public void setAddress_street(String address_street) {
+        this.address_street = address_street;
+    }
+
+    public String getAddress_city() {
+        return address_city;
+    }
+
+    public void setAddress_city(String address_city) {
+        this.address_city = address_city;
+    }
+
+    public String getPesel() {
+        return pesel;
+    }
+
+    public void setPesel(String pesel) {
+        this.pesel = pesel;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
 }
