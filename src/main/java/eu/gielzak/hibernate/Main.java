7c201652e9c2f883f7448eefce006962c646c688
Adrian Gielzak <adrian.gielzak@outlook.com>
2018-03-19 09:10:13 +0100

    first commit

---
+++ src/main/java/eu/gielzak/hibernate/Main.java
@@ -0,0 +1,10 @@
+package eu.gielzak.hibernate;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
---
+++ src/main/java/eu/gielzak/hibernate/entity/StudentsEntity.java
@@ -0,0 +1,4 @@
+package eu.gielzak.hibernate.entity;
+
+public class StudentsEntity {
+}
